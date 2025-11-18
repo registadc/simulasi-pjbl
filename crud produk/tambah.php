4b74130e08b4da323b3017752f6cf48acafe0caf
giee🦩 <cahyanidwiregista08@gmail.com>
2025-11-18 13:53:39 +0700

    commit 18 nov

--- crud produk/tambah.php
+++ crud produk/tambah.php
@@ -12,7 +12,7 @@ $query_kategori = mysqli_query($koneksi, $kategori);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah poduk</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
